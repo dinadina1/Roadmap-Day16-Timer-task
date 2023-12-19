7d7bf91ba283dfe8cfe0da5f84606f270b7fa654
Dinakaran-FSD-Developer <123656660+dinadina1@users.noreply.github.com>
2023-12-19 23:43:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Set Timer
+let a=setTimeout(()=>
+document.getElementById('header').innerText="10"
+,1000)
+let b=setTimeout(()=>
+document.getElementById('header').innerText="9"
+,2000)
+let c=setTimeout(()=>
+document.getElementById('header').innerText="8"
+,3000)
+let d=setTimeout(()=>
+document.getElementById('header').innerText="7"
+,4000)
+let e=setTimeout(()=>
+document.getElementById('header').innerText="6"
+,5000)
+let f=setTimeout(()=>
+document.getElementById('header').innerText="5"
+,6000)
+let g=setTimeout(()=>
+document.getElementById('header').innerText="4"
+,7000)
+let h=setTimeout(()=>
+document.getElementById('header').innerText="3"
+,8000)
+let i=setTimeout(()=>
+document.getElementById('header').innerText="2"
+,9000)
+let j=setTimeout(()=>
+document.getElementById('header').innerText="1"
+,10000)
+let m=setTimeout(()=>
+document.getElementById('header').innerText="Happy Independance Day"
+,11000)
+
+
+
